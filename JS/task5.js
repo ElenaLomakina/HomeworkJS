b032b494574425f1a9ba9755ec6beb2ad8855320
Lena Lomakina <lomakina.june1977@gmail.com>
2017-12-25 22:06:21 +0200

    change value

--- JS/task5.js
+++ JS/task5.js
@@ -1,59 +1,59 @@
 // ----------------------------------------------- Task 1. Exchange  ---------------------------------------------------
 
-// var cur1, cur2, sum1, sum2;
-// var rateUSD = 28;
-// var rateEUR = 33;
-//
-// var change = {
-//     "USD": {
-//         "USD": 1,
-//         "EUR": rateUSD / rateEUR,
-//         "UAH": rateUSD
-//     },
-//     "EUR": {
-//         "USD": rateEUR / rateUSD,
-//         "EUR": 1 ,
-//         "UAH": rateEUR
-//     },
-//     "UAH": {
-//         "USD": 1 / rateUSD,
-//         "EUR": 1 / rateEUR,
-//         "UAH": 1
-//     }
-// };
-//
-// var curNames = Object.keys(change);
-//
-// function exchange( from, to) {
-//     return Math.round( change[from][to] * sum1 * 100) / 100;
-// }
-//
-// cur1 = prompt("Choose the currency you want to exchange: USD, EUR, UAH");
-// cur2 = prompt("Choose the currency to which you want to change: USD, EUR, UAH");
-// sum1 = prompt("Enter your sum");
-//
-// var isCorrect1 = curNames.some(function(cur){
-//     return cur1 === cur;
-// });
-//
-// var isCorrect2 = curNames.some(function(cur){
-//     return cur2 === cur;
-// });
-//
-// var isCorrect = isCorrect1 && isCorrect2;
-//
-// if (!isCorrect) {
-//     alert("Sorry, we can not exchange this currency");
-// }
-// else{
-//     if (isNaN(Number(sum1))) {
-//         alert("Sum is not a number")
-//     }
-//     else{
-//         sum2 = exchange(cur1, cur2);
-//         alert( sum1 +" " + cur1 + " is " + sum2 + " " + cur2);
-//     }
-// }
+var cur1, cur2, sum1, sum2;
+var rateUSD = 28;
+var rateEUR = 33;
+
+var change = {
+    "USD": {
+        "USD": 1,
+        "EUR": rateUSD / rateEUR,
+        "UAH": rateUSD
+    },
+    "EUR": {
+        "USD": rateEUR / rateUSD,
+        "EUR": 1 ,
+        "UAH": rateEUR
+    },
+    "UAH": {
+        "USD": 1 / rateUSD,
+        "EUR": 1 / rateEUR,
+        "UAH": 1
+    }
+};
+
+var curNames = Object.keys(change);
+
+function exchange( from, to) {
+    return Math.round( change[from][to] * sum1 * 100) / 100;
+}
+
+cur1 = prompt("Choose the currency you want to exchange: USD, EUR, UAH");
+cur2 = prompt("Choose the currency to which you want to change: USD, EUR, UAH");
+sum1 = prompt("Enter your sum");
+
+var isCorrect1 = curNames.some(function(cur){
+    return cur1 === cur;
+});
+
+var isCorrect2 = curNames.some(function(cur){
+    return cur2 === cur;
+});
+
+var isCorrect = isCorrect1 && isCorrect2;
+
+if (!isCorrect) {
+    alert("Sorry, we can not exchange this currency");
+}
+else{
+    if (isNaN(Number(sum1))) {
+        alert("Sum is not a number")
+    }
+    else{
+        sum2 = exchange(cur1, cur2);
+        alert( sum1 +" " + cur1 + " is " + sum2 + " " + cur2);
+    }
+}
 
 
 // -----------------------------------------------  Task 2. Squad  -----------------------------------------------------
@@ -204,10 +204,6 @@ function getCloneElement(arr, index) {
 
 // Создать 2 массива, в одном из которых будут храниться единицы земельных участков, приносящих плоды, в другом - сами плоды.
 
-//  `b. Написать функцию проверки готовности каждой, либо указанной (индекс) единицы ЗР для сборки.
-//
-
-
 var farm = [];
 var harvest = [];
 
@@ -216,16 +212,19 @@ var harvest = [];
 //  Функция должна создавать объект и добавлять его в массив земельных участков.
 //      function adds resources to farm ------------------------------------------------------------------
 
-function addResourceToFarm (type, product, income, durability, seed) {
+function addResourceToFarm (type, product, cropYield, durability, seed) {
     var obj = {
         "type": type,
         "product": product,
-        "income": income,
+        "cropYield": cropYield,
         "durability": durability,
         "seed": seed
     };
     farm.push(obj);
 }
+
+//  `b. Написать функцию проверки готовности каждой, либо указанной (индекс) единицы ЗР для сборки.
+//  ???  Что влияет на готовность? Время? Откуда его брать? Установить для каждого участка? Или для каждого плода?  ???
 
 
 //  c. Написать функцию вывода информации о количестве единиц плодов, сгенерированных данным ЗР.
@@ -234,12 +233,12 @@ function addResourceToFarm (type, product, income, durability, seed) {
 function productsFromResource(index) {
     if (index === undefined) {
         return farm.reduce(function(prevVal, curVal) {
-            return prevVal + curVal.income;
+            return prevVal + curVal.cropYield;
         }, 0)
     }
     else {
         var obj = farm[index];
-        return obj.income;
+        return obj.cropYield;
     }
 }
 
@@ -272,13 +271,13 @@ function getHarvest(index) {
         return console.log("The pane was not seeded");
     }
     else if (exist){
-        exist.quantity += obj.income;
+        exist.quantity += obj.cropYield;
         obj.seed = false;
     }
     else {
         var newObj = {
             "product": obj.product,
-            "quantity": obj.income
+            "quantity": obj.cropYield
         };
         harvest.push(newObj);
         obj.seed = false;
